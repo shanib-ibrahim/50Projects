f50d93de0e0a3ded6332f80224eb46347aecc46b
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-01-20 06:54:25 +0400

    changes in random choice picker

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -1,4 +1,5 @@
-const textArea = document.getElementById("textarea");
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
 
 textArea.addEventListener("onchange", (e) => {
   console.log(e.target);
